6e4a5f498bd11e04893bacaa8451db7e62d13f45
loicmaes <production@maesloic.fr>
2024-10-27 02:11:36 +0200

    :tada: Init project
    Signed-off-by: loicmaes <production@maesloic.fr>

---
+++ app.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { IconoirProvider } from "@iconoir/vue";
+
+const iconProps = {
+  "width": "1rem",
+  "height": "1rem",
+  "stroke-width": "2",
+};
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <IconoirProvider :icon-props="iconProps">
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </IconoirProvider>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,44 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@nuxt/eslint",
+    "shadcn-nuxt",
+    "@nuxtjs/google-fonts",
+  ],
+  devtools: { enabled: true },
+  colorMode: {
+    classSuffix: "",
+  },
+  runtimeConfig: {
+    public: {
+      apiUrl: "",
+    },
+  },
+  compatibilityDate: "2024-04-03",
+  eslint: {
+    checker: true,
+    config: {
+      stylistic: {
+        indent: 2,
+        semi: true,
+        quotes: "double",
+      },
+    },
+  },
+  googleFonts: {
+    families: {
+      Montserrat: {
+        wght: "100..900",
+        ital: "100..900",
+      },
+    },
+  },
+  tailwindcss: {
+    cssPath: ["~/assets/css/tailwind.css", { injectPosition: "first" }],
+    configPath: "tailwind.config",
+    exposeConfig: false,
+    viewer: true,
+  },
+});
