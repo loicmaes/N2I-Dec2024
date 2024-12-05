13f286cd42818647ceec1429f3b6bbbd5c0f8284
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-12-06 00:01:40 +0100

    :rocket: Add utility function to generate arrays

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function generateArray(length: number) {
+  const array = [];
+  for (let i = 0; i < length; ++i) array.push(i);
+  return array;
+}
