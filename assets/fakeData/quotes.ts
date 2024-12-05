8a6158cb4a6a5dd071797ed9b9dee7784d5d7c4b
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-12-06 00:01:26 +0100

    :rocket: Update quotes in fake data

--- assets/fakeData/quotes.ts
+++ assets/fakeData/quotes.ts
@@ -3,12 +3,7 @@ import type { Quote } from "~/types/quotes";
 export default [
   {
     author: 1,
-    content: "T'es pas la touche la plus pressée du clavier.",
-    time: new Date("Dec, 5 2024 18:36"),
-  },
-  {
-    author: 0,
-    content: "J'y crois pas, j'ai poussé la DB de dev en production..!",
-    time: new Date("Dec, 5 2024 20:41"),
+    content: "\"any\" ; et pas un cheval qui hennie.",
+    time: new Date("Dec, 5 2024 18:16"),
   },
 ] as Quote[];
