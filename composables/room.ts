234b94d4fbb61c45c34bca43a6fef9bf7d21436f
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-12-06 02:10:47 +0100

    :sparkles: Add join a room feature

--- composables/room.ts
+++ composables/room.ts
@@ -1,6 +1,6 @@
 import { useLocalStorage } from "@vueuse/core";
 import type { IPlayer } from "~/types/player";
-import type { ICreateRoom, IRoom } from "~/types/room";
+import type { ICreateRoom, IJoinRoom, IRoom } from "~/types/room";
 import { buildApiUrl } from "~/lib/utils";
 
 export const useConnectedPlayers = () => useState<IPlayer[]>("connectedPlayers", () => []);
@@ -23,6 +23,36 @@ export async function createRoom(payload: ICreateRoom) {
       body: {
         ...payload,
         difficulty: payload.difficulty.toUpperCase(),
+      },
+    });
+    useLocalStorage("game-token", "").value = token;
+
+    const { players, room } = await $fetch<{
+      players: IPlayer[];
+      room: IRoom;
+    }>(buildApiUrl("/rooms/current"), {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    useRoom().value = {
+      ...room,
+      state: "idle",
+    };
+    useConnectedPlayers().value = players;
+    usePlayer().value = players[0];
+  }
+  catch (e) {
+    console.error(e);
+  }
+}
+
+export async function joinRoom(payload: IJoinRoom) {
+  try {
+    const { token } = await $fetch<{ token: string }>(buildApiUrl("/rooms/join"), {
+      method: "POST",
+      body: {
+        ...payload,
       },
     });
     useLocalStorage("game-token", "").value = token;
