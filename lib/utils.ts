01fb93b21d6f5d0bee9e2d920a4b560d7afbbb37
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-12-06 07:43:09 +0100

    :ambulance: Fix prod link

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export const buildApiUrl = (path: string) => `https://staging.api.n2i-2024.ovh.servers.dotslash.fr${path.startsWith("/") ? "" : "/"}${path}`;
+export const buildApiUrl = (path: string) => `https://api.n2i.dotslash.fr${path.startsWith("/") ? "" : "/"}${path}`;
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
